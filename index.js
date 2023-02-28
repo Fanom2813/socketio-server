a4e6b43dfe0c874e8b9a09d6c98f8d3b08e6e870
Fanom2813 <40917429+Fanom2813@users.noreply.github.com>
2023-02-28 23:18:33 +0300

    add endpoints

--- index.js
+++ index.js
@@ -1,14 +1,56 @@
-const app = require('express')();
+const express = require('express');
+const app = express();
 const server = require('http').createServer(app);
 const io = require('socket.io')(server);
 const port = process.env.PORT || 8080;
-app.get('/', function(req, res) {
-   res.send("Hi!");
+
+app.use(express.json());
+
+app.get('/', function (req, res) {
+  res.send("Hi!");
 });
 
-app.post('/send-to-user', function(req, res) {
-  res.send("Hi!");
+app.post('/send-to-user', async function (req, res) {
+
+  const sockets = await io.fetchSockets();
+  for (const socket of sockets) {
+    if (socket.userId == req.body.userId) {
+      console.log("user found ", socket.id);
+      // console.log(socket.handshake);
+      // console.log(socket.rooms);
+      // console.log(socket.data);
+
+      //delete
+      delete req.body.userId;
+      const evt = req.body.event;
+      delete req.body.event;
+
+      socket.emit(evt, req.body);
+      // socket.join(/* ... */);
+      // socket.leave(/* ... */);
+      // socket.disconnect(/* ... */);
+    }
+  }
+
+
+  res.send(
+    { status: "sent" }
+  );
 })
+
+app.post('/send-to-all', async function (req, res) {
+
+  const evt = req.body.event;
+  delete req.body.event;
+
+
+  io.emit(evt, req.body);
+
+  res.send(
+    { status: "sent" }
+  );
+})
+
 
 
 io.on('connection', (socket) => {
@@ -22,6 +64,6 @@ io.on('connection', (socket) => {
     socket.userId = data;
   });
 })
-server.listen(port, function() {
+server.listen(port, function () {
   console.log(`Listening on port ${port}`);
 });
